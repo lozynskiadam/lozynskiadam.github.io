5e9118155385910d28ce851e7e4b237ba3927128
Adam Łożynski <adam.lozynski@alterpage.pl>
2023-01-27 18:08:48 +0100

    charset

--- sprite/assets/Hero.js
+++ sprite/assets/Hero.js
@@ -8,7 +8,7 @@ class Hero {
         let image = new Image();
         image.onload = () => {
             this.#sprite = new Sprite(image, 32, 48);
-            this.#sprite.play(750, true);
+            this.#sprite.play(650, true);
             this.setDirection('south');
         };
         image.src = 'assets/charset.png';
@@ -22,7 +22,7 @@ class Hero {
     stay() {
         this.#isWalking = false;
         this.#sprite.stop();
-        this.#sprite.play(750, true);
+        this.#sprite.play(650, true);
         this.#sprite.setRow(1);
         this.#sprite.setColumn({
             south: 1,
@@ -35,7 +35,7 @@ class Hero {
     walk() {
         this.#isWalking = true;
         this.#sprite.stop();
-        this.#sprite.play(750, true);
+        this.#sprite.play(650, true);
         this.#sprite.setRow(1);
         this.#sprite.setColumn({
             south: 5,
