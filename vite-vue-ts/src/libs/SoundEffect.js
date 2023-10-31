ad6f83869b67c4a7a53a9a61356ee417a61b3bd8
Adam Łożynski <adam.lozynski@alterpage.pl>
2023-10-31 16:46:24 +0100

    Ragnoria - manifest.json

--- vite-vue-ts/src/libs/SoundEffect.js
+++ vite-vue-ts/src/libs/SoundEffect.js
@@ -3,18 +3,18 @@ import { sound } from '@pixi/sound';
 export default class SoundEffect {
 
     static load() {
-        sound.add('inventoryOpen', {url: 'sfx/inventory-open.mp3', preload: true});
-        sound.add('inventoryClose', {url: 'sfx/inventory-close.mp3', preload: true});
-        sound.add('spikes', {url: 'sfx/spikes.mp3', preload: true});
-        sound.add('click', {url: 'sfx/click.wav', preload: true});
-        sound.add('confirm', {url: 'sfx/confirm.mp3', preload: true});
-        sound.add('chest', {url: 'sfx/chest.m4a', preload: true});
-        sound.add('footstep', {url: 'sfx/footstep.m4a', preload: true});
-        sound.add('dead', {url: 'sfx/dead.m4a', preload: true});
-        sound.add('mining', {url: 'sfx/mining.m4a', preload: true});
-        sound.add('pickup', {url: 'sfx/pickup.m4a', preload: true});
-        sound.add('potion', {url: 'sfx/potion.m4a', preload: true});
-        sound.add('login', {url: 'sfx/login.m4a', preload: true});
+        sound.add('inventoryOpen', {url: 'assets/sfx/inventory-open.mp3', preload: true});
+        sound.add('inventoryClose', {url: 'assets/sfx/inventory-close.mp3', preload: true});
+        sound.add('spikes', {url: 'assets/sfx/spikes.mp3', preload: true});
+        sound.add('click', {url: 'assets/sfx/click.wav', preload: true});
+        sound.add('confirm', {url: 'assets/sfx/confirm.mp3', preload: true});
+        sound.add('chest', {url: 'assets/sfx/chest.m4a', preload: true});
+        sound.add('footstep', {url: 'assets/sfx/footstep.m4a', preload: true});
+        sound.add('dead', {url: 'assets/sfx/dead.m4a', preload: true});
+        sound.add('mining', {url: 'assets/sfx/mining.m4a', preload: true});
+        sound.add('pickup', {url: 'assets/sfx/pickup.m4a', preload: true});
+        sound.add('potion', {url: 'assets/sfx/potion.m4a', preload: true});
+        sound.add('login', {url: 'assets/sfx/login.m4a', preload: true});
         sound.volumeAll = 0.2;
     }
 
