66916d59981ee90e3dc5068c8a493a2d19837a9e
Adam Lozynski <adam.lozynski@alterpage.pl>
2024-09-10 15:19:31 +0200

    trayme cd.

--- trayme/dist/assets/NotFoundView.916f0c60.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,a as o,o as t}from"./index.1797ca3d.js";import{V as n}from"./VEmptyState.18e84b87.js";const s={};function a(r,c){return t(),o(n,{headline:"Whoops, 404",title:"Page not found",text:"The page you were looking for does not exist",image:"https://vuetifyjs.b-cdn.net/docs/images/logos/v.png"})}const p=e(s,[["render",a]]);export{p as default};
--- trayme/dist/assets/ShoppingListView.b59a31f4.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as n}from"./index.1797ca3d.js";const o={};function t(s,r){return n(),c("div",null,"shopping list")}const _=e(o,[["render",t]]);export{_ as default};
